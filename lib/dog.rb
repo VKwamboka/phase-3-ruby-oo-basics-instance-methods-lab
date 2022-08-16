50b352c9e9eb9ad0028e97179e55e7bf693998d6
verah <verahnyagaka101@gmail.com>
2022-08-16 10:48:06 +0300

    commit tree 0e9ff027d9e6df2995a1d9f0c7cc694a60876616

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+# Create Objects  
+  fido = Dog.new
+
+# Call Methods
+  fido.bark
+  fido.sit
+
